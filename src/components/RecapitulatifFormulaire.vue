e347ab47cfaba1798287ed702856de34390eadc6
yann <yann.boisselier@gmail.com>
2025-06-20 16:21:05 +0200

    fix recap

--- src/components/RecapitulatifFormulaire.vue
+++ src/components/RecapitulatifFormulaire.vue
@@ -8,49 +8,57 @@
       <!-- Adresse -->
       <div>
         <h3 class="font-semibold">Adresse</h3>
-        <div>{{ adresse.label || adresse }}</div>
+        <div>
+          {{
+            typeof props.adresse === 'object' && props.adresse !== null
+              ? props.adresse.label
+              : props.adresse
+          }}
+        </div>
       </div>
       <!-- Type de bien -->
       <div>
         <h3 class="font-semibold">Informations principales</h3>
         <ul class="ml-4 list-disc">
-          <li>Type : {{ typeBien.type }}</li>
-          <li>Surface : {{ typeBien.surface }} m²</li>
-          <li>Année de construction : {{ typeBien.annee }}</li>
-          <li>Diagnostic : {{ diagnosticLabel(typeBien.diagnostic) }}</li>
-          <li>État : {{ etatLabel(typeBien.etat) }}</li>
+          <li>Type : {{ props.typeBien.type }}</li>
+          <li>Surface : {{ props.typeBien.surface }} m²</li>
+          <li>Année de construction : {{ props.typeBien.annee }}</li>
+          <li>Diagnostic : {{ diagnosticLabel(props.typeBien.diagnostic) }}</li>
+          <li>État : {{ etatLabel(props.typeBien.etat) }}</li>
         </ul>
       </div>
       <!-- Précisions -->
       <div>
         <h3 class="font-semibold">Précisions</h3>
         <ul class="ml-4 list-disc">
-          <li>Nombre de pièces : {{ precisions.pieces }}</li>
-          <li>Nombre de chambres : {{ precisions.chambres }}</li>
-          <li>Nombre de salles de bain : {{ precisions.sdb }}</li>
-          <li>Nombre d'étages dans l'immeuble : {{ precisions.etagesImmeuble }}</li>
-          <li>Étage de ce bien : {{ precisions.etageBien }}</li>
+          <li>Nombre de pièces : {{ props.precisions.pieces }}</li>
+          <li>Nombre de chambres : {{ props.precisions.chambres }}</li>
+          <li>Nombre de salles de bain : {{ props.precisions.sdb }}</li>
+          <li>Nombre d'étages dans l'immeuble : {{ props.precisions.etagesImmeuble }}</li>
+          <li>Étage de ce bien : {{ props.precisions.etageBien }}</li>
         </ul>
       </div>
       <!-- Caractéristiques -->
       <div>
         <h3 class="font-semibold">Caractéristiques</h3>
         <ul class="ml-4 list-disc">
-          <li v-for="(v, k) in caracteristiques" :key="k" v-if="v">
+          <li v-for="k in caracteristiquesCochees as string[]" :key="k">
             {{ caracteristiqueLabel(k) }}
           </li>
-          <li v-if="!hasCaracteristique">Aucune</li>
+          <li v-if="caracteristiquesCochees.length === 0">Aucune</li>
         </ul>
       </div>
       <!-- À propos -->
       <div>
         <h3 class="font-semibold">À propos de vous</h3>
         <ul class="ml-4 list-disc">
-          <li>Nom : {{ aPropos.nom }}</li>
-          <li>Prénom : {{ aPropos.prenom }}</li>
-          <li>Téléphone : {{ aPropos.telephone }}</li>
-          <li>Email : {{ aPropos.email }}</li>
-          <li>Consentement : <span v-if="aPropos.consent">Oui</span><span v-else>Non</span></li>
+          <li>Nom : {{ props.aPropos.nom }}</li>
+          <li>Prénom : {{ props.aPropos.prenom }}</li>
+          <li>Téléphone : {{ props.aPropos.telephone }}</li>
+          <li>Email : {{ props.aPropos.email }}</li>
+          <li>
+            Consentement : <span v-if="props.aPropos.consent">Oui</span><span v-else>Non</span>
+          </li>
         </ul>
       </div>
     </div>
@@ -58,7 +66,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps } from 'vue'
+import { computed } from 'vue'
 
 interface AdresseSuggestion {
   label?: string
@@ -78,9 +86,6 @@ interface PrecisionsBien {
   etagesImmeuble?: number | null
   etageBien?: number | null
 }
-interface Caracteristiques {
-  [key: string]: boolean
-}
 interface APropos {
   nom?: string
   prenom?: string
@@ -89,15 +94,13 @@ interface APropos {
   consent?: boolean
 }
 
-defineProps<{
+const props = defineProps<{
   adresse: AdresseSuggestion | string
   typeBien: TypeBien
   precisions: PrecisionsBien
-  caracteristiques: Caracteristiques
+  caracteristiques: Record<string, boolean>
   aPropos: APropos
 }>()
-
-const props = defineProps()
 
 function diagnosticLabel(val: string) {
   if (val === 'unknown') return 'Je ne sais pas'
@@ -130,7 +133,9 @@ const caracteristiqueLabels: Record<string, string> = {
 function caracteristiqueLabel(key: string) {
   return caracteristiqueLabels[key] || key
 }
-const hasCaracteristique = computed(() => {
-  return Object.values(props.caracteristiques).some((v: unknown) => Boolean(v))
-})
+const caracteristiquesCochees = computed<string[]>(() =>
+  Object.entries(props.caracteristiques)
+    .filter(([k, v]) => !!k && v === true)
+    .map(([k]) => k),
+)
 </script>
